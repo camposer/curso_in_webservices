1ae2be383da46bb6e7fa3cbd76029f52896e735d
Rodolfo Campos <camposer@gmail.com>
2014-09-16 14:42:09 +0200

    Implementación del ejercicio REST de Calculadora

---
+++ Ejercicio1Servidor/src/to/CalculadoraResultadoTo.java
@@ -0,0 +1,49 @@
+package to;
+
+import enumaration.Operacion;
+
+public class CalculadoraResultadoTo {
+	private float op1;
+	private float op2;
+	private Operacion operacion;
+	private float resultado;
+	
+	public CalculadoraResultadoTo() {
+	
+	}
+	
+	public CalculadoraResultadoTo(float op1, float op2, Operacion operacion, float resultado) {
+		this.op1 = op1;
+		this.op2 = op2;
+		this.operacion = operacion;
+		this.resultado = resultado;
+		
+	}
+
+	public float getOp1() {
+		return op1;
+	}
+	public void setOp1(float op1) {
+		this.op1 = op1;
+	}
+	public float getOp2() {
+		return op2;
+	}
+	public void setOp2(float op2) {
+		this.op2 = op2;
+	}
+	public Operacion getOperacion() {
+		return operacion;
+	}
+	public void setOperacion(Operacion operacion) {
+		this.operacion = operacion;
+	}
+	public float getResultado() {
+		return resultado;
+	}
+	public void setResultado(float resultado) {
+		this.resultado = resultado;
+	}
+
+	
+}
---
+++ Ejercicio1Servidor/src/webservice/CalculadoraRest.java
@@ -0,0 +1,49 @@
+package webservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import enumaration.Operacion;
+import service.ICalculadoraService;
+import to.CalculadoraResultadoTo;
+
+public class CalculadoraRest implements ICalculadoraRest {
+	@Autowired
+	private ICalculadoraService calculadoraService;
+
+	@Override
+	public CalculadoraResultadoTo restar(float a, float b) {
+		return new CalculadoraResultadoTo(
+				a, 
+				b, 
+				Operacion.RESTAR, 
+				calculadoraService.restar(a, b));
+	}
+
+	@Override
+	public CalculadoraResultadoTo dividir(float a, float b) {
+		return new CalculadoraResultadoTo(
+				a, 
+				b, 
+				Operacion.DIVIDIR, 
+				calculadoraService.dividir(a, b));
+	}
+
+	@Override
+	public CalculadoraResultadoTo sumar(float a, float b) {
+		return new CalculadoraResultadoTo(
+				a, 
+				b, 
+				Operacion.SUMAR, 
+				calculadoraService.sumar(a, b));
+	}
+
+	@Override
+	public CalculadoraResultadoTo multiplicar(float a, float b) {
+		return new CalculadoraResultadoTo(
+				a, 
+				b, 
+				Operacion.MULTIPLICAR, 
+				calculadoraService.multiplicar(a, b));
+	}
+
+}
