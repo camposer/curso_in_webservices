22024f9d62e861539e8a6dfa832c0d98ed0fbaad
Rodolfo Campos <camposer@gmail.com>
2014-09-17 10:39:35 +0200

    Solución del ejercicio 2 (incluye API Rest - sólo servidor)

---
+++ Ejercicio2Servidor/src/webservice/IPersonaRest.java
@@ -0,0 +1,92 @@
+package webservice;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import to.PersonaTo;
+
+// Mapping /personas
+@Consumes("application/json")
+@Produces("application/json")
+public interface IPersonaRest {
+	/**
+	 * GET http://localhost:8080/Ejercicio2Servidor/ws/personas
+	 * Accept: application/json
+	 * 
+	 * [ {...}, {...}]
+	 */
+	@GET
+	@Path("")
+	public List<PersonaTo> obtenerPersonas();
+
+	/**
+	 * GET http://localhost:8080/Ejercicio2Servidor/ws/personas/1
+	 * Accept: application/json
+	 * 
+	 * {...}
+	 */
+	@GET
+	@Path("/{id}")
+	public PersonaTo obtenerPersona(@PathParam("id") Integer id);
+	
+	/**
+	 * GET http://localhost:8080/Ejercicio2Servidor/ws/personas/1
+	 * Accept: application/json
+	 * 
+	 * null
+	 */
+	@GET
+	@Path("/order/nombre")
+	public List<PersonaTo> obtenerPersonasOrdenadasPorNombre();
+
+	/**
+	 * POST http://localhost:8080/Ejercicio2Servidor/ws/personas
+	 * Content-type: application/json
+	 * {
+     *  "nombre": "9",
+     *  "apellido": "9",
+     *  "altura": 9,
+     *  "fechanacimiento": "2009-09-09"
+	 * }
+	 *
+	 * void (No content)
+	 */	
+	@POST
+	@Path("")
+	public void agregarPersona(PersonaTo pTo);
+	
+
+	/**
+	 * PUT http://localhost:8080/Ejercicio2Servidor/ws/personas/1
+	 * Content-type: application/json
+	 * {
+     *  "nombre": "9",
+     *  "apellido": "9",
+     *  "altura": 9,
+     *  "fechanacimiento": "2009-09-09"
+	 * }
+	 *
+	 * void (No content)
+	 */	
+	@PUT
+	@Path("/{id}")
+	public void modificarPersona(@PathParam("id") Integer id, PersonaTo pTo);
+	
+	
+	/**
+	 * DELETE http://localhost:8080/Ejercicio2Servidor/ws/personas/1
+	 * 
+	 * void (No content)
+	 */
+	@DELETE
+	@Path("/{id}")
+	public void eliminarPersona(@PathParam("id") Integer id);
+}
---
+++ Ejercicio2Servidor/src/webservice/PersonaRest.java
@@ -0,0 +1,107 @@
+package webservice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Ordenador;
+import model.Persona;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import service.IPersonaService;
+import to.OrdenadorTo;
+import to.PersonaTo;
+
+public class PersonaRest implements IPersonaRest {
+	@Autowired
+	private IPersonaService personaService;
+
+	@Override
+	public List<PersonaTo> obtenerPersonas() {
+		List<Persona> personas = personaService.obtenerPersonas();
+		List<PersonaTo> personasTo = new ArrayList<PersonaTo>();
+		
+		for (Persona p : personas) 
+			personasTo.add(obtenerPersonaTo(p));
+		
+		return personasTo;
+	}
+
+	private PersonaTo obtenerPersonaTo(Persona p) {
+		PersonaTo pTo = new PersonaTo();
+		pTo.setId(p.getId());
+		pTo.setNombre(p.getNombre());
+		pTo.setApellido(p.getApellido());
+		pTo.setAltura(p.getAltura());
+		pTo.setFechanacimiento(p.getFechanacimiento());
+		
+		List<Ordenador> ordenadores = p.getOrdenadores();
+		List<OrdenadorTo> ordenadoresTo = new ArrayList<OrdenadorTo>();
+		for (Ordenador o : ordenadores) {
+			OrdenadorTo oTo = new OrdenadorTo();
+			oTo.setId(o.getId());
+			oTo.setNombre(o.getNombre());
+			oTo.setSerial(o.getSerial());
+			
+			ordenadoresTo.add(oTo);
+		}
+		
+		pTo.setOrdenadores(ordenadoresTo);
+		
+		return pTo;
+	}
+
+	private Persona obtenerPersona(PersonaTo pTo) {
+		Persona p = new Persona();
+		p.setId(pTo.getId());
+		p.setNombre(pTo.getNombre());
+		p.setApellido(pTo.getApellido());
+		p.setAltura(pTo.getAltura());
+		p.setFechanacimiento(pTo.getFechanacimiento());
+		
+		List<Ordenador> ordenadores = new ArrayList<Ordenador>();
+		List<OrdenadorTo> ordenadoresTo = pTo.getOrdenadores();
+		if (ordenadoresTo != null) for (OrdenadorTo oTo : ordenadoresTo) {
+			Ordenador o = new Ordenador();
+			o.setId(oTo.getId());
+			o.setNombre(oTo.getNombre());
+			o.setSerial(oTo.getSerial());
+			
+			ordenadores.add(o);
+		}
+		
+		p.setOrdenadores(ordenadores);
+		
+		return p;
+	}
+
+	@Override
+	public void agregarPersona(PersonaTo pTo) {
+		personaService.agregarPersona(obtenerPersona(pTo));
+	}
+
+	@Override
+	public PersonaTo obtenerPersona(Integer id) {
+		Persona persona = personaService.obtenerPersona(id);
+		
+		return obtenerPersonaTo(persona);
+	}
+
+	@Override
+	public void eliminarPersona(Integer id) {
+		personaService.eliminarPersona(id);
+	}
+
+	@Override
+	public List<PersonaTo> obtenerPersonasOrdenadasPorNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modificarPersona(Integer id, PersonaTo pTo) {
+		pTo.setId(id);
+		personaService.modificarPersona(obtenerPersona(pTo));
+	}
+
+}
